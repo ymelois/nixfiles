9e52ed48b46e2956ae70c739a44a0fe7101218e2
Youn Mélois <youn@melois.dev>
2026-04-22 18:18:03 +0200

    fix(gaming): FileChooser not installed wrap workaround for prismlauncher
    Signed-off-by: Youn Mélois <youn@melois.dev>

--- home-manager/modules/gaming/prismlauncher.nix
+++ home-manager/modules/gaming/prismlauncher.nix
@@ -14,21 +14,37 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      (prismlauncher.override {
-        additionalPrograms = [
-          ffmpeg
-          vlc
-          alsa-oss
-        ];
+    home.packages =
+      let
+        prism = pkgs.prismlauncher.override {
+          additionalPrograms = with pkgs; [
+            ffmpeg
+            vlc
+            alsa-oss
+          ];
 
-        jdks = [
-          graalvm25-ce
-          graalvm21-ce
-          jdk17
-          jdk8
-        ];
-      })
-    ];
+          jdks = with pkgs; [
+            graalvm25-ce
+            graalvm21-ce
+            jdk17
+            jdk8
+          ];
+        };
+      in
+      [
+        # Inject GTK gsettings schemas so the file picker does not crash with
+        # "Settings schema 'org.gtk.Settings.FileChooser' is not installed".
+        # See https://github.com/PrismLauncher/PrismLauncher/issues/4595.
+        (pkgs.symlinkJoin {
+          name = "prismlauncher-with-schemas-${prism.version}";
+          paths = [ prism ];
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/prismlauncher \
+              --prefix XDG_DATA_DIRS : "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+          '';
+          inherit (prism) meta;
+        })
+      ];
   };
 }
